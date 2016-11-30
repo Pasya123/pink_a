cc44a47f08b13ecc0b3cf4e4aae7eab29fb0f268
Nikolai <rus.nick@mail.ru>
2016-11-30 10:58:38 +0300

    Добавлена графика для мобильной версии

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var svgmin = require("gulp-svgmin");
+var svgstore = require("gulp-svgstore");
+var inject = require("gulp-inject");
+var rename = require("gulp-rename");
+
 
 gulp.task("style", function() {
   gulp.src("sass/style.sass")
@@ -18,6 +23,27 @@ gulp.task("style", function() {
     ]))
     .pipe(gulp.dest("css"))
     .pipe(server.stream());
+});
+
+gulp.task("svg-store", function() {
+  return gulp.src("img/svg-img/**/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg : true
+    }))
+    .pipe(rename("symbols.svg"))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task('svg-inject', function () {
+
+    function fileContents (filePath, file) {
+        return file.contents.toString();
+    }
+
+    return gulp.src("*.html")
+        .pipe(inject(gulp.src("img/symbols.svg"), { transform: fileContents }))
+        .pipe(gulp.dest("."));
 });
 
 gulp.task("serve", ["style"], function() {
