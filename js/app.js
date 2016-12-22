e0ccbccf427b9fc3fe57786d60c133522ab3d539
Nikolai <rus.nick@mail.ru>
2016-12-22 18:05:22 +0300

    Добавлены дилоговые окна + правки по верстке

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 document.body.classList.remove("nojs");
 
+/* Меню навигации */
 
 var mainNav = document.querySelector(".main-nav");
 
@@ -13,3 +14,46 @@ function toggleMainNav() {
 
 mainNavOpen.addEventListener("click", toggleMainNav);
 mainNavClose.addEventListener("click", toggleMainNav);
+
+
+/* Диалоговые окна */
+var successDialog = document.querySelector(".modal-dialog--success");
+var failureDialog = document.querySelector(".modal-dialog--failure");
+
+function closeBtnOnClick(evt) {
+
+  if (evt.target.classList.contains("modal-dialog__close-btn")) {
+    evt.currentTarget.classList.remove("modal-dialog--show");
+  }
+}
+
+if (successDialog) {
+  successDialog.addEventListener("click", closeBtnOnClick);
+}
+
+if (failureDialog) {
+  failureDialog.addEventListener("click", closeBtnOnClick);
+}
+
+
+/* Форма */
+var contestForm = document.querySelector(".contest-form");
+
+
+if (contestForm) {
+
+  var contestFormSubmitBtn = contestForm.querySelector("input[type=submit]");
+
+  contestFormSubmitBtn.addEventListener("click", function(evt) {
+    evt.preventDefault();
+
+    if (successDialog && contestForm.checkValidity()) {
+      successDialog.classList.add("modal-dialog--show");
+    }
+
+    if (failureDialog && !contestForm.checkValidity()) {
+      failureDialog.classList.add("modal-dialog--show");
+    }
+  });
+}
+
